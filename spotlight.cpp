55e28574f7d33e3fec9d38ecca49ad6f8ff82a09
imane0101010 <86524680+imane0101010@users.noreply.github.com>
2021-10-22 15:42:10 +0100

    Update spotlight.cpp

--- spotlight.cpp
+++ spotlight.cpp
@@ -30,6 +30,36 @@ Spotlight::Spotlight(string filename,int X,int Y):Image(filename),x(X),y(Y),file
 
 }
 void Spotlight::changeSpotPoint(int centerX, int centerY){
-  Spotlight(filename,centerX,centerY);
+ 
+
+    for(unsigned X = 0; X < width()  ; X++){
+       for(unsigned Y = 0; Y < height(); Y++)
+       {
+          //reference on the pixel
+          HSLAPixel &P = getPixel(X, Y);
+
+               double distance1=sqrt((x-X)*(x-X)+(y-Y)*(y-Y));
+               double distance=sqrt((X-centerX)*(X-centerX)+(Y-centerY)*(Y-centerY));
+               double decrease = 1-distance*0.5/100 ;
+               double increase = 1-distance1*0.5/100 ;
+               if (distance1>160) {
+                   increase=0.2;
+}
+
+
+                P.l =P.l/increase;
+
+
+               if (distance>160) {
+                   decrease=0.2;
+               }
+
+               P.l = decrease*P.l;
+
+               }
+
+
+             }
 
 }
+
